1f2228d88270ff87b0cc6def5577dc043957d677
Romel Alejandro Hernandez Resendiz <romelresndiz@gmail.com>
2020-08-22 12:29:43 -0500

    Added CSV READER

--- 001_downloader.py
+++ 001_downloader.py
@@ -1,29 +1,29 @@
+from datetime import datetime
 import requests
-
 # CONSTANTS
 URL_BASE='https://api.github.com/'
-
-#Functions
+# FUNCTIONS
 def get_github_user(username):
-    url= f'{URL_BASE}users/{username}'
+    url = f'{URL_BASE}users/{username}'
     response = requests.get(url)
     if response.status_code == 200:
-       return response.json()
+        return response.json()
     return None
-
 def download_user_avatar(image_url):
-    response=requests.get(image_url)
+    response = requests.get(image_url)
     if response.status_code == 200:
         response_content = response.content
-        filename = 'tmp/useravatar.png'
+        filename = f'tmp/{image_filename()}.png'
         with open(filename, 'wb') as image: 
             image.write(response_content)
             return filename
     return None
-
-username= input('Give the username:\t')
-user= get_github_user(username)
-
+def image_filename():
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    return timestamp
+username = input('Give the username:\t')
+user = get_github_user(username)
 if user:
     user_avatar_url = user.get('avatar_url')
     download_user_avatar(user_avatar_url)
